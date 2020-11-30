7d270046ed4c7ad0fc65fd3b606f5cbc120abc89
Kai Dong <dongkai981@gmail.com>
2020-11-30 11:51:14 -0600

    Updated readme response

--- index.js
+++ index.js
@@ -53,19 +53,7 @@ function createReadme(response) {
   Welcome to my portfolio page, you will find my biography with links to linkedIn, Github, and my resume in the About page. Past projects with repository links can be found on the Portfolio page and you will find a place to enter you contact info in the Contact page. This portfolio is a work in progress, more will get added in the future.
   
   ### Implementation
-  * Utilize semantic elements in HTML.
-  * Use bootstrap to style portfolio, include usage of navbar, sticky footer, forms, grids and rows.
-  * Use bootstrap's column sizing to eliminate media query usage.
-  * jQuery for repo link click functions. 
-  * Validate HTML with W3C Validator Service to ensure there are no errors.
-  
-  [Portfolio Webpage Link](https://kaidong-chr.github.io/My_Portfolio/)
-  
-  ### Portfolio Demo
-  
-  ![Image](./assets/Images/My_Portfolio.gif "Portfolio Demo")
-  
-  ![Image](https://img.shields.io/badge/Languages-html%20%7C%20css%20%7C%20javascript-yellow)
+  ${response.implementation}
   
   ### Installation
   ${response.installation}
@@ -73,6 +61,6 @@ function createReadme(response) {
   ### License
   
   ![badge](https://img.shields.io/badge/license-${answers.license}-brightgreen)<br />
-  ';
-  fs.writeFile("answer.html", htmlContent, error => error ? console.log(error) : console.log("Yay!"));
+  `;
+  fs.writeFileAsync("./file/README.md", readMe, error => error ? console.log(error) : console.log("README generated!"));
 }
