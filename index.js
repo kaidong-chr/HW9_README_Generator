4ffdd5308a82ef3c664a976c40ac0dcf8e1caf23
Kai Dong <dongkai981@gmail.com>
2020-11-30 11:46:43 -0600

    Added response for readme

--- index.js
+++ index.js
@@ -23,7 +23,19 @@ inquirer
       name: "installation",
       message: "Describe the installation process.",
     },
-
+    {
+      type: "list",
+      name: "license",
+      message: [
+        "Apache",
+        "Academic",
+        "GNU",
+        "ISC",
+        "MIT",
+        "Mozilla",
+        "Open"
+      ]
+    }
   ])
   .then((response) => {
     console.log(response);
@@ -33,5 +45,34 @@ inquirer
     console.log(err);
   });
 function createReadme(response) {
+  let readMe ="";
+  readMe += `
+  # ${response.projectTitle}
 
+  ### Portfolio Page
+  Welcome to my portfolio page, you will find my biography with links to linkedIn, Github, and my resume in the About page. Past projects with repository links can be found on the Portfolio page and you will find a place to enter you contact info in the Contact page. This portfolio is a work in progress, more will get added in the future.
+  
+  ### Implementation
+  * Utilize semantic elements in HTML.
+  * Use bootstrap to style portfolio, include usage of navbar, sticky footer, forms, grids and rows.
+  * Use bootstrap's column sizing to eliminate media query usage.
+  * jQuery for repo link click functions. 
+  * Validate HTML with W3C Validator Service to ensure there are no errors.
+  
+  [Portfolio Webpage Link](https://kaidong-chr.github.io/My_Portfolio/)
+  
+  ### Portfolio Demo
+  
+  ![Image](./assets/Images/My_Portfolio.gif "Portfolio Demo")
+  
+  ![Image](https://img.shields.io/badge/Languages-html%20%7C%20css%20%7C%20javascript-yellow)
+  
+  ### Installation
+  ${response.installation}
+  
+  ### License
+  
+  ![badge](https://img.shields.io/badge/license-${answers.license}-brightgreen)<br />
+  ';
+  fs.writeFile("answer.html", htmlContent, error => error ? console.log(error) : console.log("Yay!"));
 }
